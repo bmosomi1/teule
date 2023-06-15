d4e5e43d9c27666a775a1c497db32b7afbed5f9c
Benard Mosomi <benardmosomi@gmail.com>
2023-06-15 16:40:52 +0300

    flats

--- sms/views.py
+++ sms/views.py
@@ -2368,18 +2368,10 @@ def house_statement_preview(request, client_id):
     else:
         statement =TeuleStatement.objects.filter(id=0)
         context = {        
-            'statement': statement,
-                    
+            'statement': statement,       
             
-            'names': names,
-            'tel': tel,
-            'client_id': client_id,
             
-            'flat': flat,
-            'client_num': client_num,
-            'house': house,
             'client': client
-        }
         return render(request, 'sms/house_statement_preview.html', context)
         
 
